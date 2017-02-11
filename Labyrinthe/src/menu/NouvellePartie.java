e38b9346907362a525fa3d60b49e5ca88e0a8019
RiverWilliams <julie.roche59@sfr.fr>
2017-02-11 18:34:32 +0100

    amélioration menu

--- Labyrinthe/src/menu/NouvellePartie.java
+++ Labyrinthe/src/menu/NouvellePartie.java
@@ -8,6 +8,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import sauvegarde.Sauvegarde_Chargement;
 import javafx.scene.control.TextInputDialog;
 
 
@@ -19,26 +20,49 @@ public class NouvellePartie {
 		Scene nouv = new Scene(root);
 		nouv.setFill(Color.BLACK);
 		
-		Text text = new Text("test joueur ok");
-		text.setFill(Color.WHITE);
-		text.setFont(new Font(30));
-		text.setX(30.0);
-		text.setY(30.0);
-		
+		//on demande au joueur de saisir un nom
 		TextInputDialog selectNom = new TextInputDialog();  
 		selectNom.setTitle("Nouvelle Partie");  
 		selectNom.setHeaderText("Joueur");  
 		selectNom.setContentText("Nom du joueur");  
 		Optional<String> nom = selectNom.showAndWait();  
 		nom.ifPresent(guestString -> { 
+			 String joueur = nom.get();
 		     selectNom.close();
-		     String joueur = nom.get();
 		     System.out.println(nom);
 		     System.out.println(joueur);
+		     //si le nom existe déjà, le joueur peut en saisir un autre ou charger la partie
+		     try {
+				if (! Sauvegarde_Chargement.nouvelle_sauvegarde(joueur)){
+					Text text = new Text("Nom de joueur déjà pris, voulez vous ");
+					text.setFont(new Font(25));
+					text.setFill(Color.WHITE);
+					text.setX(30.0);
+					text.setY(30.0);
+					Text t2 = new Text(" • Charger la partie correspondante ?");
+					t2.setFont(new Font(25));
+					t2.setFill(Color.WHITE);
+					t2.setX(35.0);
+					t2.setY(70.0);
+					//t2.setOnMouseClicked(mouseEvent -> Menu.launch());
+					Text t3 = new Text(" • Choisir un autre nom ?");
+					t3.setFont(new Font(25));
+					t3.setFill(Color.WHITE);
+					t3.setX(35.0);
+					t3.setY(110.0);
+					t3.setOnMouseClicked(mouseEvent -> NouvellePartie.newGame(stage));
+					root.getChildren().add(text);
+					root.getChildren().add(t2);
+					root.getChildren().add(t3);
+					}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		});
 		
-		root.getChildren().add(text);
+		
 		stage.setScene(nouv);
+		//root.getChildren().add(e)
 
 		
 	}
